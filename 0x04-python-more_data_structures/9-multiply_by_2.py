82737ee57d402e951588e31ca6155985d8e8c9bd
Thato <thatomsimanga@gmail.com>
2023-12-06 14:18:37 -0800

    0-square_matrix_simple.py

---
+++ 0x04-python-more_data_structures/9-multiply_by_2.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+def multiply_by_2(a_dictionary):
+    new_dir = a_dictionary.copy()
+    list_key + list(new_dir.key())
+
+    for i in list_key:
+        new_dir[i] *= 2
+
+        return (new_dir)
